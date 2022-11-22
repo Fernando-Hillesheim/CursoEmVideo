66c2bb3aadc7648b79c7d3c12788dbcb74f449e6
Marataca <95379276+Marataca@users.noreply.github.com>
2022-11-22 14:05:49 -0300

    Update D39.py

--- Python/Mundo2/D39.py
+++ Python/Mundo2/D39.py
@@ -3,4 +3,11 @@ from math import trunc
 ano = int(input('Qual ano você nasceu?\n'))
 agora = time()/(3600*24*365) + 1970
 idade = trunc(agora - ano)
+aux = idade - 18
 
+if aux == 0:
+    print('Você deve se alistar esse ano')
+elif aux < 0:
+    print('Você deve se alistar daqui a {} anos'.format(-1*aux))
+else:
+    print('Você devia ter se alistado há {} anos'.format(aux))
